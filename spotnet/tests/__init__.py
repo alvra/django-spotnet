7861e5f9262d7e282bcd5e3d1c528c8cc322ecb4
Alexander van Ratingen <alexandervanratingen@gmail.com>
2012-06-01 15:11:13 +0200

    Updated the TravisCI config file so it actually works.

--- spotnet/tests/__init__.py
+++ spotnet/tests/__init__.py
@@ -4,3 +4,8 @@ import unittest
 def suite():
     import os
     return unittest.defaultTestLoader.discover(os.path.dirname(__file__), '*.py')
+
+
+if __name__ == '__main__':
+    # this would never work unless DJANGO_SETTINGS_MODULE is defined
+    unittest.TextTestRunner(verbosity=2).run(suite())
