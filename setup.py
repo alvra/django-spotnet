672fbca7bbba0984ce20818f793adcb8a7245bf0
Alexander van Ratingen <alexandervanratingen@gmail.com>
2012-06-10 16:20:24 +0200

    Made the setup script pep8 compliant. Also added pypy as CI testing platform.

--- setup.py
+++ setup.py
@@ -1,26 +1,27 @@
 from distutils.core import setup
 
+
 setup(
-    name = 'django-spotnet',
-    version = '0.1',
-    description = 'An full-featured application for Django to handle spotnet posts.',
-    author = 'Alexander van Ratingen',
-    author_email = 'alexander@van-ratingen.nl',
-    url = 'http://www.bitbucket.org/Blue/django-spotnet/',
-    download_url = 'http://www.bitbucket.org/Blue/django-spotnet/',
-    packages = [
+    name='django-spotnet',
+    version='0.1',
+    description='An full-featured application for Django to handle spotnet posts.',
+    author='Alexander van Ratingen',
+    author_email='alexander@van-ratingen.nl',
+    url='http://www.bitbucket.org/Blue/django-spotnet/',
+    download_url='http://www.bitbucket.org/Blue/django-spotnet/',
+    packages=[
         'spotnet',
         'spotnet.downloadserver',
         'spotnet.management',
         'spotnet.management.commands',
         'spotnet.tests',
     ],
-    package_data = {'spotnet': [
+    package_data={'spotnet': [
         'templates/spotnet/*.html',
         'static/*',
         'locale/*/LC_MESSAGES/*',
     ]},
-    classifiers = [
+    classifiers=[
         'Development Status :: 4 - Beta',
         'Environment :: Web Environment',
         'Framework :: Django',
@@ -32,4 +33,3 @@ setup(
         'Topic :: Utilities',
     ],
 )
-
